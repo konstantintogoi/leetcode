854ab229606776c65a7ebdbf1546f068233bf4aa
Konstantin Togoi <konstantin.togoi@gmail.com>
2021-02-24 23:03:30 +0300

    update 'Sqrt(x)' problem

--- problems/0069_sqrtx.py
+++ problems/0069_sqrtx.py
@@ -6,14 +6,30 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         """Sqrt(x).
 
+        >>> Solution().mySqrt(0)
+        0
+        >>> Solution().mySqrt(1)
+        1
+        >>> Solution().mySqrt(2)
+        1
+        >>> Solution().mySqrt(3)
+        1
         >>> Solution().mySqrt(4)
         2
         >>> Solution().mySqrt(8)
         2
+        >>> Solution().mySqrt(9)
+        3
 
         """
         root = 0
-        while root * root < x:
+
+        while (root + 1) * (root + 1) <= x:
             root += 1
-        return root - (root * root != x)
+            shift = 2
+            while (root + shift) * (root + shift) <= x:
+                root += shift
+                shift *= 2
+
+        return root
 
