b53271b917e3942841d57a82a740563505baca90
Konstantin Togoi <konstantin.togoi@gmail.com>
2021-03-11 08:56:53 +0300

    add 'Short Encoding of Words' problem

---
+++ problems/0820_short_encoding_of_words.py
@@ -0,0 +1,32 @@
+"""
+Solution of "Short Encoding of Words" problem at
+https://leetcode.com/problems/short-encoding-of-words/
+"""
+from typing import List
+
+
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        """Short Encoding.
+
+        >>> Solution().minimumLengthEncoding(['time', 'me', 'bell'])
+        10
+        >>> Solution().minimumLengthEncoding(['t'])
+        2
+        >>> Solution().minimumLengthEncoding(['me', 'time'])
+        5
+        >>> Solution().minimumLengthEncoding(['time', 'atime', 'btime'])
+        12
+
+        """
+        curword = ''
+        encword = ''
+        words.sort(key=lambda x: -len(x))
+        for word in words:
+            curword = word + '#'
+            if curword in encword:
+                continue
+            else:
+                encword += curword
+        return len(encword)
+
