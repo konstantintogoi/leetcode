2905e702cc92340c0b8a2af3c2e5be526d20108c
Konstantin Togoi <konstantin.togoi@gmail.com>
2021-03-12 20:17:54 +0300

    add 'Coin Change' problem

--- README.md
+++ README.md
@@ -27,6 +27,7 @@ python -m doctest 0001_two_sum.py
 |  623 | Medium     | Add One Row to Tree                                 |
 |  575 | Easy       | Distribute Candies                                  |
 |  416 | Medium     | Partition Equal Subset Sum                          |
+|  322 | Medium     | Coin Change                                         |
 |  268 | Easy       | Missing Number                                      |
 |  246 | Easy       | Strobogrammatic Number                              |
 |  191 | Easy       | Number of 1 Bits                                    |
---
+++ problems/0322_coin_change.py
@@ -0,0 +1,43 @@
+"""
+Solution of "Coin Change" problem at
+https://leetcode.com/problems/coin-change/
+"""
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        """Coin Change.
+
+        >>> Solution().coinChange([1, 2, 5], 11)
+        3
+        >>> Solution().coinChange([2], 3)
+        -1
+        >>> Solution().coinChange([1], 0)
+        0
+        >>> Solution().coinChange([1], 1)
+        1
+        >>> Solution().coinChange([1], 2)
+        2
+        >>> Solution().coinChange([2, 5, 10, 1], 27)
+        4
+        >>> Solution().coinChange([186, 419, 83, 408], 6249)
+        20
+
+        """
+        memo = {}
+
+        def dp(mnt):
+            if mnt < 0:
+                return float('inf')
+            if mnt in memo:
+                return memo[mnt]
+            if mnt in coins:
+                return 1
+
+            memo[mnt] = min(dp(mnt - m) + 1 for m in coins)
+            return memo[mnt]
+
+        ncoins = dp(amount) if amount else 0
+        return -1 if ncoins == float('inf') else ncoins
+
