c6a14dbd810de1c9627f366ade49ce513c93271c
Konstantin Togoi <konstantin.togoi@gmail.com>
2021-03-19 23:53:34 +0300

    add 'Keys and Rooms' problem

---
+++ problems/0841_keys_and_rooms.py
@@ -0,0 +1,30 @@
+"""
+Solution of "Keys and Rooms" problem at
+https://leetcode.com/problems/keys-and-rooms/
+"""
+from typing import List
+
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        """Can Visit All Rooms.
+
+        >>> Solution().canVisitAllRooms([[1], [2], [3], []])
+        True
+        >>> Solution().canVisitAllRooms([[1, 3], [3, 0, 1], [2], [0]])
+        False
+        >>> Solution().canVisitAllRooms([[1], [2], [], [3]])
+        False
+
+        """
+        colors = {}
+
+        def dfs(i):
+            if i in colors: return
+            colors[i] = 'gray'
+            for j in rooms[i]: dfs(j)
+            colors[i] = 'black'
+
+        dfs(0)
+        return len(colors) == len(rooms)
+
