9296ad90b69e9a3efc12cdea70778ac25e53e821
Konstantin Togoi <konstantin.togoi@gmail.com>
2021-04-21 01:17:01 +0300

    add a hard problem - 'Critical Connections in a Network'

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ python -m doctest 0001_two_sum.py
 | 1332 | Easy       | Remove Palindromic Subsequences                       |
 | 1302 | Medium     | Deepest Leaves Sum                                    |
 | 1198 | Medium     | Find Smallest Common Element in All Rows              |
+| 1192 | Hard       | Critical Connections in a Network                     |
 | 1165 | Easy       | Single-Row Keyboard                                   |
 | 1136 | Medium     | Parallel Courses                                      |
 | 1074 | Hard       | Number of Submatrices That Sum to Target              |
---
+++ problems/1192_critical_connections_in_a_network.py
@@ -0,0 +1,49 @@
+"""
+Solution of the hard problem - "Critical Connections in a Network",
+https://leetcode.com/problems/critical-connections-in-a-network/
+"""
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def criticalConnections(
+        self,
+        n: int,
+        connections: List[List[int]],
+    ) -> List[List[int]]:
+        """Critical Connections.
+
+        >>> Solution().criticalConnections(
+        ...     4, [[0, 1], [1, 2], [2, 0], [1, 3]]
+        ... )
+        [[1, 3]]
+        >>> Solution().criticalConnections(
+        ...     6, [[0, 1], [1, 2], [2, 0], [1, 3], [3, 4], [4, 5], [5, 3]]
+        ... )
+        [[1, 3]]
+
+        """
+        edges = defaultdict(list)
+        for a, b in connections:
+            edges[a].append(b)
+            edges[b].append(a)
+
+        ans = []
+        disc, low = [0] * n, [0] * n
+
+        def dfs(curr, prev, time):
+            disc[curr] = low[curr] = time
+
+            for nxt in edges[curr]:
+                if not disc[nxt]:
+                    dfs(nxt, curr, time + 1)
+                    low[curr] = min(low[curr], low[nxt])
+                elif nxt != prev:
+                    low[curr] = min(low[curr], disc[nxt])
+                if low[nxt] > disc[curr]:
+                    ans.append([curr, nxt])
+
+        dfs(0, -1, 1)
+        return ans
+
