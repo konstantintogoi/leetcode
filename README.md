de10f85b5f279d09e2a2ccfa5051dfadfd1ad937
Konstantin Togoi <konstantin.togoi@gmail.com>
2021-05-17 20:44:39 +0300

    add a medium problem - 'Generate Random Point in a Circle'

--- README.md
+++ README.md
@@ -64,6 +64,7 @@ python -m doctest 0001_two_sum.py
 |  536 | Medium     | Construct Binary Tree from String                       |
 |  535 | Medium     | Encode and Decode TinyURL                               |
 |  509 | Easy       | Fibonacci Number                                        |
+|  478 | Medium     | Generate Random Point in a Circle                       |
 |  474 | Medium     | Ones and Zeroes                                         |
 |  423 | Medium     | Reconstruct Original Digits from English                |
 |  417 | Medium     | Pacific Atlantic Water Flow                             |
---
+++ problems/0478_generate_random_point_in_a_circle.py
@@ -0,0 +1,28 @@
+"""
+Solution of the medium problem - "Generate Random point in a Circle",
+https://leetcode.com/problems/generate-random-point-in-a-circle/
+"""
+from math import cos, pi, sin, sqrt
+from random import uniform
+from typing import List
+
+
+class Solution:
+    def __init__(self, radius: float, x_center: float, y_center: float):
+        self.r = radius
+        self.x = x_center
+        self.y = y_center
+
+    def randPoint(self) -> List[float]:
+        """Generates random point.
+
+        >>> solution = Solution(1.0, 0.0, 0.0)
+        >>> solution.randPoint()  # doctest: +SKIP
+        >>> solution.randPoint()  # doctest: +SKIP
+        >>> solution.randPoint()  # doctest: +SKIP
+
+        """
+        a = uniform(0, 2 * pi)
+        r = self.r * sqrt(uniform(0, 1))
+        return self.x + r * cos(a), self.y + r * sin(a)
+
