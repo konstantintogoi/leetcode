f9b05df5a90891850f138a8ef9d1c2f11be450c0
Konstantin Togoi <konstantin.togoi@gmail.com>
2021-08-04 22:54:03 +0300

    update docstring of the 1396-th problem

--- problems/1396_design_underground_system.py
+++ problems/1396_design_underground_system.py
@@ -1,5 +1,5 @@
 """
-Solution of "Design Underground System" problem at
+Solution of the medium problem - "Design Underground System",
 https://leetcode.com/problems/design-underground-system/
 """
 class UndergroundSystem:
