0b2b5e232cb2ed0a9d225c8969d29169e2ffe3bf
Konstantin Togoi <konstantin.togoi@gmail.com>
2021-08-20 16:53:55 +0300

    add a hard problem - 'First Missing Positive'

--- README.md
+++ README.md
@@ -113,6 +113,7 @@ python -m doctest 0001_two_sum.py
 |   46 | Medium     | Permutations                                            |
 |   45 | Medium     | Jump Game II                                            |
 |   43 | Medium     | Multiply Strings                                        |
+|   41 | Hard       | First Missing Positive                                  |
 |   40 | Medium     | Combination Sum II                                      |
 |   39 | Medium     | Combination Sum                                         |
 |   38 | Easy       | Count and Say                                           |
---
+++ problems/0041_first_missing_positive.py
@@ -0,0 +1,33 @@
+"""
+Solution of the hard problem - "First Missing Positive",
+https://leetcode.com/problems/first-missing-positive/
+"""
+from typing import List
+
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        """First Missing Positive.
+
+        >>> Solution().firstMissingPositive([2147483647])
+        1
+        >>> Solution().firstMissingPositive([1, 2, 0])
+        3
+        >>> Solution().firstMissingPositive([3, 4, -1, 1])
+        2
+        >>> Solution().firstMissingPositive([7, 8, 9, 11, 12])
+        1
+
+        """
+        maxnum = 0
+        posnums = set()
+
+        for num in nums:
+            if num > 0:
+                posnums.add(num)
+                maxnum = max(num, maxnum)
+
+        for i in range(1, maxnum + 2):
+            if i not in posnums:
+                return i
+
