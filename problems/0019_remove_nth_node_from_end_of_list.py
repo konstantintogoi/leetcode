b18ddbf32449842166b21c9b133698aea8fa713c
Konstantin Togoi <konstantin.togoi@gmail.com>
2021-09-20 17:33:18 +0300

    update docstring of the 19-th problem

--- problems/0019_remove_nth_node_from_end_of_list.py
+++ problems/0019_remove_nth_node_from_end_of_list.py
@@ -1,6 +1,7 @@
 """
-Solution of the medium problem - "Remove Nth Node From End of List",
+Solution of the medium problem
 https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+"Remove Nth Node From End of List"
 """
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -53,4 +54,3 @@ class Solution:
             node = node.next
 
         return head
-
