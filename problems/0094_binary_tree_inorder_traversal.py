87dcfd7aec0e49de9261f371a071efd68b09c317
Konstantin Togoi <konstantin.togoi@gmail.com>
2021-11-01 17:57:47 +0300

    update docstring of the 94-th problem

--- problems/0094_binary_tree_inorder_traversal.py
+++ problems/0094_binary_tree_inorder_traversal.py
@@ -1,6 +1,7 @@
 """
-Solution of the easy problem - "Binary Tree Inorder Traversal",
+Solution of the easy problem
 https://leetcode.com/problems/binary-tree-inorder-traversal/
+"Binary Tree Inorder Traversal"
 """
 from typing import List
 
@@ -60,4 +61,3 @@ class Solution:
         self.bfs(node.right, table)
 
         return table
-
