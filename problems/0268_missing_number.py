befb8eb162b31ff23470cb8396909e322f8046cf
Konstantin Togoi <konstantin.togoi@gmail.com>
2021-11-19 18:03:36 +0300

    update docstring of the 268-th problem

--- problems/0268_missing_number.py
+++ problems/0268_missing_number.py
@@ -1,6 +1,7 @@
 """
-Solution of the easy problem - "Missing Number",
+Solution of the easy problem
 https://leetcode.com/problems/missing-number/
+"Missing Number"
 """
 from typing import List
 
@@ -20,4 +21,3 @@ class Solution:
 
         """
         return len(nums) * (len(nums) + 1) // 2 - sum(nums)
-
