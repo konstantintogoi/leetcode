4cdbcb769077352124a95597010691863dc3e621
Konstantin Togoi <konstantin.togoi@gmail.com>
2021-12-06 18:11:12 +0300

    update docstring of the 478-th problem

--- problems/0478_generate_random_point_in_a_circle.py
+++ problems/0478_generate_random_point_in_a_circle.py
@@ -1,6 +1,7 @@
 """
-Solution of the medium problem - "Generate Random point in a Circle",
+Solution of the medium problem
 https://leetcode.com/problems/generate-random-point-in-a-circle/
+"Generate Random point in a Circle"
 """
 from math import cos, pi, sin, sqrt
 from random import uniform
@@ -25,4 +26,3 @@ class Solution:
         a = uniform(0, 2 * pi)
         r = self.r * sqrt(uniform(0, 1))
         return self.x + r * cos(a), self.y + r * sin(a)
-
