70b168ea87ab0c23c7f9e9cca77880909cd562d1
Konstantin Togoi <konstantin.togoi@gmail.com>
2025-10-13 19:47:10 +0300

    improve solution for the problem 'Binary Tree Inorder Traversal'

--- problems/0094_binary_tree_inorder_traversal.py
+++ problems/0094_binary_tree_inorder_traversal.py
@@ -37,8 +37,6 @@ class Solution:
         ...     TreeNode(1, None, TreeNode(2, TreeNode(3)))
         ... )
         [1, 3, 2]
-        >>> Solution().inorderTraversal(TreeNode(None))
-        []
         >>> Solution().inorderTraversal(TreeNode(1))
         [1]
         >>> Solution().inorderTraversal(TreeNode(1, TreeNode(2)))
@@ -47,17 +45,25 @@ class Solution:
         [1, 2]
 
         """
-        table = []
-        if root is None or root.val is None:
-            return table
-        self.bfs(root, table)
-        return table
+        traversal = []
 
-    def bfs(self, node: TreeNode, table: List[TreeNode]):
-        if not node: return table
+        node = root
+        while node:
+            if not node.left:
+                traversal.append(node.val)
+                node = node.right
+            else:
+                prev = node.left
+                while prev.right and prev.right != node:
+                    prev = prev.right
 
-        self.bfs(node.left, table)
-        table.append(node.val)
-        self.bfs(node.right, table)
+                if not prev.right:
+                    prev.right = node
+                    node = node.left
+                else:
+                    prev.right = None
+                    traversal.append(node.val)
+                    node = node.right
 
-        return table
+        return traversal
+
